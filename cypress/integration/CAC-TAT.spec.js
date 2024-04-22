3343d597e2f9b2ed97addbb331305ea62729ae27
unknown <dennisdesousa.ds@gmail.com>
2024-04-22 09:00:10 -0300

    entendendo check-box

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -6,15 +6,9 @@
 // https://on.cypress.io/writing-first-test
 
 /// <reference types="Cypress" />
-
-/*describe('Central de Atendimento ao Cliente TAT', function(){
-    it('verificar titulo da aplicação', function(){
-        cy.visit('./src/index.html') /// acessa o site
-        cy.title().should('be.equal','Central de Atendimento ao Cliente TAT' )  /// valida se o titulo é igual ao esperado
-        cy.get()
-        .should('be.visible')  /// espera que o elemento
-        .type("Tecnologia Avantiz") ///
-    })
+/*
+describe('Central de Atendimento ao Cliente TAT', function(){
+    
 })*/
 
 describe('Central de Atendimento ao cliente 2.0', function(){
@@ -22,16 +16,43 @@ describe('Central de Atendimento ao cliente 2.0', function(){
         cy.visit('./src/index.html')    
     })
 
-    it('inserir texto na aplicação', function(){
+    it('verificar titulo da aplicação', function(){
+        cy.title().should('be.equal','Central de Atendimento ao Cliente TAT' )  /// valida se o titulo é igual ao esperado
+        /*cy.get()
+            .should('be.visible')  /// espera que o elemento
+            .type("Tecnologia Avantiz") //*/
+    })
+
+    it('preenche os campos obrigatórios e envia o formulário', function(){
         cy.get('#firstName').type('Dennis')
         cy.get('#lastName').type('Sousa')
         cy.get('#email').type('dennis@gmail.com')
         cy.get('#phone').type('15987654321')
-        cy.get('#product').select('Blog').click
+        cy.get('#product').select('Cursos').click
+        cy.get('#product').should('have.value', 'cursos')
         cy.get('input[value="elogio"]').check()
         cy.get('#email-checkbox').check()
         cy.get('#open-text-area')
             .type('Olá, estou feliz com o meu atendimento no seu produto.  Obrigado pela atenção.')
-        cy.get('.button').click
+        cy.get('.button').click()            
+    })
+
+    it('Mensagem de sucesso', function(){
+        cy.get('.success > strong').should('contain', 'Mensagem enviada com sucesso.')
+    })
+
+    it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', function(){
+        cy.get('#firstName').type('Dennis')
+        cy.get('#lastName').type('Sousa')
+        cy.get('#email').type('dennisgmailcom')
+        cy.get('#phone').type('15987654321')
+        cy.get('#product').select('Cursos').click
+        cy.get('input[value="elogio"]').check()
+        cy.get('#email-checkbox').check()
+        cy.get('#email-checkbox').uncheck()
+        cy.get('#open-text-area')
+            .type('Olá, estou feliz com o meu atendimento no seu produto.  Obrigado pela atenção.')
+        cy.get('.button').click()
+        cy.get('.error').should('be.visible')
     })
 })
